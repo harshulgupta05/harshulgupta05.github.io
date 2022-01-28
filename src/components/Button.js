c8312649456f1435a269eba5e116091dc629a996
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2022-01-28 14:56:41 -0500

    Delete src directory

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-function Button(props) {
-    return (
-        <div>
-            <a href={"#" + props.buttonlink} className="btn btn-primary m-5">{props.buttonname}</a>
-        </div>
-    );
-}
-
-export default Button;
